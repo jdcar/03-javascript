0675c97bb1667c4c6884b7c5555fa695fccfe0ab
jdcar <jdcarlstone@gmail.com>
2020-11-16 09:21:43 -0600

    Write javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,88 @@
 
-// Prompt for password
+// Prompts for password criteria
 
-var password = "password";
+function generatePassword() {
 
-var passwordLength = prompt("What length should the password be? Choose a number between 8 and 128");
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const numbers = "0123456789"
+  const specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-if (passwordLength >= 8 && passwordLength <= 128) {
-  console.log("Password is correct length")
+  let password = "";
 
-} else {
-  prompt("Please enter a number between 8 and 128")
-  console.log("Password does not meet criteria")
+  let passwordLength = prompt("What length should the password be? Choose a number between 8 and 128");
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log("Password is correct length")
+    console.log(passwordLength)
+  } else {
+    prompt("Please enter a number between 8 and 128")
+    console.log("Password does not meet criteria")
+  }
+
+  var passwordLowerCase = prompt("Should password include lowercase letters? Type any key to say yes, for no leave blank.");
+
+  if (passwordLowerCase) {
+    password = password + lowerCaseLetters
+    console.log(password)
+    console.log("Password will include lower case")
+  } else {
+    console.log("Password will not include lower case")
+  }
+
+  var passwordUpperCase = prompt("Should password include upper case letters? Type any key to say yes, for no leave blank.");
+
+  if (passwordUpperCase) {
+    password = password + upperCaseLetters
+    console.log(password)
+    console.log("Password will include upper case")
+  } else {
+    console.log("Password will not include upper case")
+  }
+
+  var passwordNumbers = prompt("Should password include numbers? Type any key to say yes, for no leave blank.")
+
+  if (passwordNumbers) {
+    password = password + numbers
+    console.log(password)
+    console.log("Password will include numbers")
+  } else {
+    console.log("No numbers needed")
+  }
+
+  var passwordSpecialChars = prompt("Should password include special characters? Special characters include:\n !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+
+  if (passwordSpecialChars) {
+    password = password + specialCharacters
+    console.log(password)
+    console.log("Password will include special characters")
+  } else {
+    console.log("No special characters needed")
+  }
+
+  // Now the variable "password" is made up of all the characters that can be used
+
+
+  // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  // https://www.codegrepper.com/code-examples/r/random+letter+from+string+javascript
+  var text = "";
+  for (var i = 0; i < passwordLength; i++)
+    text += password.charAt(Math.floor(Math.random() * password.length));
+  console.log(text)
+  alert("Password generated!")
+  return text;
+// At this point the generated password is the variable text
+
+  var generateBtn = document.querySelector("#generate");
+
 }
 
-var passwordLowerCase = prompt("Should password include lowercase letters?");
 
-if (passwordLowerCase.toLowerCase() == "yes") {
-  includeLower = true;
-  console.log("Password will include lower case")
-} else {
-  console.log("Password will not include lower case")
-}
-
-var passwordUpperCase = prompt("Should password include upper case letters?");
-
-if (passwordUpperCase.toLowerCase() == "yes") {
-  includeUpper = true;
-  console.log("Password will include upper case")
-} else {
-  console.log("Password will not include upper case")
-}
-
-var passwordSpecialChars = prompt("Enter any special characters you would like to include:\n !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
-
-if (passwordSpecialChars) {
-  console.log(password + passwordSpecialChars)
-} else {
-  console.log("No special characters needed")
-}
-
-
-
-
-// lowercase, uppercase, numeric, and/or special characters
+// Refractor code starts here
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// this puts the password generated into the box where it now says "Your secure Passord"
 
 
 // Write password to the #password input
